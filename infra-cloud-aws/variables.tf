9a40decb8806ba1a600526a324583e9bb706fa99
Edson L. B. Filho <edcastanha@gmail.com>
2025-07-13 14:15:15 -0300

    fix: Ajusta deploy tests

--- infra-cloud-aws/variables.tf
+++ infra-cloud-aws/variables.tf
@@ -1,3 +1,26 @@
-variable "bucket_name" {
-  type = string
+variable "environment" {
+  description = "O ambiente de implantação (ex: dev, prod)."
+  type        = string
+}
+
+variable "aws_region" {
+  description = "A região da AWS para implantar os recursos."
+  type        = string
+}
+
+variable "db_username" {
+  description = "O nome de usuário para o banco de dados RDS."
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "O nome do banco de dados RDS."
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "A classe de instância para o banco de dados RDS."
+  type        = string
+  default     = "db.t3.micro"
 }
